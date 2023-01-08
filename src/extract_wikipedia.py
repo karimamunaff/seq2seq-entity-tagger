8e10fb0e2ddada261a8be12c947a260d584f757e
karimvision <kabdulmunaff@brandwatch.com>
2023-01-08 09:00:19 -0500

    extract links from articles, save redirects separately, need to map articles to redirects

--- src/extract_wikipedia.py
+++ src/extract_wikipedia.py
@@ -49,24 +49,46 @@ def iterate_articles(xmlfile: BZ2File) -> WikipediaArticle:
             yield WikipediaArticle(current_title, content.attrib["title"], True)
 
 
-def save_articles(articles_collection: List[Dict], save_index: int) -> None:
-    save_filename = WIKIPEDIA_PROCESSED_DIRECTORY / f"articles_{save_index}.json"
+def save_articles(
+    articles_collection: List[Dict], save_index: int, save_prefix: str = "articles"
+) -> None:
+    save_filename = WIKIPEDIA_PROCESSED_DIRECTORY / f"{save_prefix}_{save_index}.json"
     _LOGGER.info(f"saving to file {save_filename}")
     with open(save_filename, "w+") as f:
         json.dump(articles_collection, f)
+
+
+def save_intermediate(
+    articles_collection: List[Dict], redirects_collection: List[Dict], save_index: int
+) -> None:
+    save_articles(articles_collection, save_index)
+    save_articles(
+        redirects_collection,
+        save_index,
+        save_prefix="redirects",
+    )
 
 
 def extract_wikipedia_dump(save_every: int = 1000, max_articles: int = 10000) -> None:
     progress_bar = tqdm()
     progress_bar.set_description("Processing Wikipedia Articles ...")
     articles_collection = []
+    redirects_collection = []
     with BZ2File(WIKIPEDIA_BZ2_XML_FILE, "r") as xmlfile:
         for index, article in tqdm(enumerate(iterate_articles(xmlfile))):
-            articles_collection.append(article.__dict__)
+            if article.is_redirect:
+                redirects_collection.append(article.__dict__)
+            else:
+                articles_collection.append(article.__dict__)
             if index % save_every == 0 and index > 0:
                 _LOGGER.info(f"Processed {index} wikipedia articles")
-                save_articles(articles_collection, int(index / save_every))
+                save_intermediate(
+                    articles_collection,
+                    redirects_collection,
+                    save_index=int(index / save_every),
+                )
                 articles_collection = []
+                redirects_collection = []
             if index > max_articles:
                 break
             progress_bar.update(1)
